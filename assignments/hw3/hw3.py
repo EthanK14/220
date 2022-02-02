7ea53081a1f9592c7672a6de6525bc4feef509f1
EthanK <kidwellea@g.cofc.edu>
2022-02-02 17:41:14 -0500

    homework 3 WIP

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,22 +1,32 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Ethan Kidwell
+hw3.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: We must use loops to find average of grades, total amount of tips, compute square roots,
+print a sequence, and calculate pi using a specified amount of terms.
 
 Certification of Authenticity:
-<include one of the following>
+
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def average():
-    pass
+    grade_amount = eval(input("How many grades will you enter? "))
+    grade_total = 0
+    for grade in range(grade_amount):
+        grade_entered = eval(input("Enter your grade: "))
+        grade_total = grade_total + grade_entered
+    grade_average = grade_total / grade_amount
+    print('Your average is: ', grade_average)
 
 
 def tip_jar():
-    pass
+    total_tips = 0
+    for tips in range(5):
+        tip_amount = eval(input("How much you would like to donate? "))
+        total_tips = total_tips + tip_amount
+    print("Total tips: $", total_tips)
 
 
 def newton():
