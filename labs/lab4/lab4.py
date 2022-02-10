b48ef768464b3a2465b0dfdad3e8ce7f529cdc50
EthanK <kidwellea@g.cofc.edu>
2022-02-10 17:43:06 -0500

    Lab 3 done

--- labs/lab4/lab4.py
+++ labs/lab4/lab4.py
@@ -10,61 +10,61 @@ I certify that this assignment is entirely my own work.
 
 from graphics import *
 
-# Creates the window for the card and set color
-win = GraphWin("Valentines Day Card <3", 500, 600)
-win.setBackground("pink")
-# Creates text object for the card
-valentines_text = Text(Point(250, 50), "<3 HAPPY VALENTINES DAY <3")
 
-# Reused points for the different shapes
-middle_point = 225  # Controls the height of the heart as a whole
-end_point = middle_point + 150  # Saves the y coordinate for the bottom in relation to the middle point
-circle_1_centers = Point(210, middle_point)
-circle_2_center = Point(290, middle_point)
+def greeting_card():
+    # Creates the window for the card and set color
+    win = GraphWin("Valentines Day Card <3", 500, 600)
+    win.setBackground("pink")
+    # Creates text object for the card
+    valentines_text = Text(Point(250, 50), "<3 HAPPY VALENTINES DAY <3")
 
-# Creates all the different shapes for the heart to be made
-# Creates the first circle for the heart
-circle_1 = Circle(circle_1_centers, 50)
-circle_1.setFill("red")
-circle_1.setOutline("red")
+    # Reused points for the different shapes
+    middle_point = 225  # Controls the height of the heart as a whole
+    end_point = middle_point + 150  # Saves the y coordinate for the bottom in relation to the middle point
+    circle_1_centers = Point(210, middle_point)
+    circle_2_center = Point(290, middle_point)
 
-# Creates the second circle
-circle_2 = Circle(circle_2_center, 50)
-circle_2.setFill("red")
-circle_2.setOutline("red")
+    # Creates all the different shapes for the heart to be made
+    # Creates the first circle for the heart
+    circle_1 = Circle(circle_1_centers, 50)
+    circle_1.setFill("red")
+    circle_1.setOutline("red")
 
-# Creates the triangle to tie the heart together
-triangle = Polygon(Point(175, middle_point), Point(325, middle_point), Point(250, end_point))
-triangle.setFill("red")
-triangle.setOutline("red")
-triangle.setWidth(35)
+    # Creates the second circle
+    circle_2 = Circle(circle_2_center, 50)
+    circle_2.setFill("red")
+    circle_2.setOutline("red")
 
-# Creates the location for the click to exit message
-end_text = Text(Point(250, 550), "")
+    # Creates the triangle to tie the heart together
+    triangle = Polygon(Point(175, middle_point), Point(325, middle_point), Point(250, end_point))
+    triangle.setFill("red")
+    triangle.setOutline("red")
+    triangle.setWidth(35)
 
-# Where the arrow is created
-arrow_shaft = Rectangle(Point(0, 400), Point(210, 408))
-arrow_shaft.setFill("black")
-arrow_head = Polygon(Point(200, 410), Point(200, 398), Point(225, 404))
-arrow_head.setFill("black")
+    # Creates the location for the click to exit message
+    end_text = Text(Point(250, 550), "")
 
+    # Where the arrow is created
+    arrow_shaft = Rectangle(Point(0, 400), Point(210, 408))
+    arrow_shaft.setFill("black")
+    arrow_head = Polygon(Point(200, 410), Point(200, 398), Point(225, 404))
+    arrow_head.setFill("black")
 
-# Where everything is drawn to the window
-arrow_shaft.draw(win)
-arrow_head.draw(win)
-valentines_text.draw(win)
-circle_1.draw(win)
-circle_2.draw(win)
-triangle.draw(win)
-end_text.draw(win)
+    # Where everything is drawn to the window
+    arrow_shaft.draw(win)
+    arrow_head.draw(win)
+    valentines_text.draw(win)
+    circle_1.draw(win)
+    circle_2.draw(win)
+    triangle.draw(win)
+    end_text.draw(win)
 
+    # Arrow Animation
+    for arrow in range(9):
+        time.sleep(.5)
+        arrow_shaft.move(15, -10)
+        arrow_head.move(15, -10)
 
-# Arrow Animation
-for arrow in range(9):
-    time.sleep(.5)
-    arrow_shaft.move(15, -10)
-    arrow_head.move(15, -10)
-
-# Displays the click to end text
-end_text.setText("Click Anywhere to End")
-win.getMouse()
+    # Displays the click to end text
+    end_text.setText("Click Anywhere to End")
+    win.getMouse()
