31ce210a634c01476ee7eebc78391e4c44a4a983
EthanK <kidwellea@g.cofc.edu>
2022-02-16 21:50:29 -0500

    hw 5 WIP

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -1,30 +1,61 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Ethan Kidwell
+hw5.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: To print a name from first, last to last, first, to split a domain name,
+to print the initials of a specified number of students, print the initials of a list of names
+entered from the user, ask a user the number of sentences to input and
+then get the sentences and then output every third character in the sentences,
+take a sentence and calculate the average word length, and a function that takes
+sentences and stories and moves the first letter to the end of the word and follows with ay.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def name_reverse():
-    pass
+    name = input("Enter a name (first last):")  # takes name from user
+    name_input = name.split(' ')  # splits name into a list
+
+    # adds the first and last name to a new list but in a reverse order
+    last_name = [name_input[1], name_input[0]]
+
+    # joins the new formatted list into a string separated by a comma
+    name_form = ', '.join(last_name)
+    print(name_form)
 
 
 def company_name():
-    pass
+    domain_name = input("Enter a domain name:")
+    print(domain_name[4:-4])  # excludes first four and last four characters
 
 
 def initials():
-    pass
+    num_students = eval(input("How many students are in the class?"))
+    for i in range(num_students):
+        student_name = input("What is the name of the student?")
+        first_initial = student_name[0]
+        space = student_name.find(" ")
+        last_initial = space + 1
+        print(first_initial + student_name[last_initial])
 
 
 def names():
-    pass
+    name = input("Enter a list of names: ")  # gets the list of names
+    name_list = name.split(', ')  # creates that into a list
+    initial_list = []  # accumulator variable for initials
+    # loop to get all initals from name list
+    for first_last in name_list:
+        full_name = first_last  # sets the name to string value
+        space = full_name.rfind(" ")  # finds the last name index value
+        first_initial = full_name[0]  # gets the first initial
+        last_initial = full_name[space + 1]  # gets the last initial
+        initial_final = first_initial + last_initial  # adds the first and last initial together
+        initial_list.append(initial_final)  # adds the initial to accumulator list
+    print(initial_list)
+
+
 
 
 def thirds():
