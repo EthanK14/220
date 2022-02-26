844a06c0210a175672ddafa6d9dd05529e664e52
EthanK <kidwellea@g.cofc.edu>
2022-02-25 22:48:54 -0500

    Homework 6 done

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -1,42 +1,87 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Ethan Kidwell
+hw6.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Make a function that converts a cash amount to a dollar amount,
+encodes a message taken from the user, calculates the area and volume
+of a sphere by taking the radius as an argument, and make a better
+encoder based on a key.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
+from math import pi
 
 def cash_converter():
-    pass
+    cash = eval(input("Enter a integer:"))
+    cash = float(cash)
+    dollar = "${:.2f}".format(cash)
+    print(dollar)
 
 
 def encode():
-    pass
+    message = input("Enter a message:")
+    key = eval(input("Enter a key:"))
+    new_message = ""
+    for character in message:
+        char = ord(character)
+        new_val = key + char
+        new_char = chr(new_val)
+        new_message = new_message + new_char
+    print(new_message)
 
 
 def sphere_area(radius):
-    pass
+    s_area = ((radius ** 2) * pi) * 4
+    return s_area
 
 
 def sphere_volume(radius):
-    pass
+    volume = ((radius ** 3) * pi) * (4/3)
+    return volume
 
 
 def sum_n(number):
-    pass
+    num = 0
+    for i in range(number + 1):
+        num = num + i
+    return num
 
 
 def sum_n_cubes(number):
-    pass
+    num = 0
+    for i in range(number + 1):
+        num = num + (i ** 3)
+    return num
 
 
 def encode_better():
-    pass
+    message = input("Enter a message: ")
+    keyword = input("Enter a keyword: ")
+    new_message = ""
+    key_len = len(keyword)  # gets length of keyword to loop through it
+    acc = 0
+    # message = message.upper()
+    # keyword = keyword.upper()
+    # message = message.replace(" ", "")
+    # keyword = keyword.replace(" ", "")
+
+    for character in message:
+        accu = acc % key_len  # doesn't allow it to ge out of range of slice
+        mess_val = ord(character)
+        key_char = keyword[accu]  # pulls the character of the keyword
+        key_val = ord(key_char)
+        mess_dif = mess_val - 65
+        difference = key_val - 65
+        mod_dif = (mess_dif + difference) % 58
+        new_int = mod_dif + 65
+        new_char = chr(new_int)
+        new_message = new_message + new_char
+        acc = acc + 1  # accumulator part
+    print(new_message)
+
+
 
 
 if __name__ == '__main__':
