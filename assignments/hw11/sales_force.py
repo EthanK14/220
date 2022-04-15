ff106002b7051198f6022a2d5fe8202e8c60bb8e
ethank14 <ethankidwell14@gmail.com>
2022-04-15 16:44:42 -0400

    hw 11

--- assignments/hw11/sales_force.py
+++ assignments/hw11/sales_force.py
@@ -20,41 +20,46 @@ class SalesForce:
         """
         self.sales_people = []
 
+    def __add_employee(self, employee_list):
+        """
+        edits a list of employees and takes each employee adds them to a list within the
+        sales_people list. Then takes and splits their id #, name into their own elements
+        and takes their sales and splits them into their own list within the employee's
+        list. it then takes all this data and adds it all of it to create and return a list
+        of employee objects.
+        """
+
+        # loops through list of sales people
+        for num in range(len(employee_list)):  # loops through the first element of the list
+            employee_list[num] = [employee_list[num]]  # adds that element to a list within the list
+            # splits the name and id and sales into their own elements
+
+            employee_list[num] = employee_list[num][0].split(', ')
+            # splits all the sales into their own elements
+            employee_list[num][2] = employee_list[num][2].split(' ')
+            # creates them all as employee objects
+            employee = SalesPerson(employee_list[num][0], employee_list[num][1])
+            for sale in range(len(employee_list[num][2])):
+                employee.enter_sale(employee_list[num][2][sale])  # adds sales to employee object
+            self.sales_people.append(employee)
+
+
     def add_data(self, file_name):
         """
         Imports data from a file with the information of sales people and
         adds it all into a list
         """
         employee_file = open(file_name, 'r')
-        for employee in employee_file.readlines():
-            self.sales_people.append(employee[:-1])
-
-    def __add_employee(self):
-        """
-        edits the sales_people list and takes each employee adds them to a list within the
-        sales_people list. Then takes and splits their id #, name into their own elements
-        and takes their sales and splits them into their own list within the employee's
-        list. it then takes all this data and adds it all of it to create and return a list
-        of employee objects.
-        """
         employee_list = []
-        # loops through list of sales people
-        for num in range(len(self.sales_people)):  # loops through the first element of the list
-            self.sales_people[num] = [self.sales_people[num]]  # adds that element to a list within the list
-            # splits the name and id and sales into their own elements
-            self.sales_people[num] = self.sales_people[num][0].split(', ')
-            # splits all the sales into their own elements
-            self.sales_people[num][2] = self.sales_people[num][2].split(' ')
-            # creates them all as employee objects
-            employee = SalesPerson(self.sales_people[num][0], self.sales_people[num][1])
-            acc = 2  # accumulates to ensure all sales are added
-            acc_2 = 0
-            for sale in range(len(self.sales_people[num][2])):
-                employee.enter_sale(self.sales_people[num][2][acc_2])  # adds sales to employee object
-                acc = acc + 1
-                acc_2 = acc_2 + 1
-            employee_list.append(employee)
-        return employee_list
+        for employee in employee_file.readlines():
+            employee_list.append(employee[:-1])
+        self.sales_people.append(self.__add_employee(employee_list))
+        none_index = self.sales_people.index(None)
+        self.sales_people.pop(none_index)
+
+    def print_sales_force(self):
+        for employee in self.sales_people:
+            print(employee)
 
     def quota_report(self, quota):
         """
@@ -63,14 +68,18 @@ class SalesForce:
         """
         employee_list = []
         # loops through list of sales people
-        for employee in self.__add_employee():  # loops through the first element of the list
+
+        # for num in range(len(self.sales_people) - 1):
+        for employee in self.sales_people:  # loops through the first element of the list
             employee_total = employee.total_sales()
+
             if employee_total >= quota:
-                employee_name = [employee.get_id(), employee.get_name(), True]
+                employee_name = [employee.get_id(), employee.get_name(), employee.total_sales(), True]
                 employee_list.append(employee_name)
             elif employee_total < quota:
-                employee_not = [employee.get_id(), employee.get_name(), False]
+                employee_not = [employee.get_id(), employee.get_name(), employee.total_sales(), False]
                 employee_list.append(employee_not)
+
         return employee_list
 
     def top_seller(self):
@@ -78,13 +87,33 @@ class SalesForce:
         returns a list of the top most seller and in the case of a tie it should
         return all those with that value
         """
-        pass
+        top_seller_sales = 0
+        seller_list = [SalesPerson(123, 'JOHN DOE')]
+        for employee in self.sales_people:
+            employee_sales = employee.total_sales()
+            if top_seller_sales < employee_sales:
+                seller_list.pop(0)
+                seller_list.insert(0 ,employee)
+
+                top_seller_sales = employee.total_sales()
+            elif top_seller_sales == employee_sales:
+
+                seller_list.append(employee)
+
+        return seller_list
+
+
 
     def individual_sales(self, employee_id):
         """
-        Returns the sales of the given id or none if the id does not exist
+        Returns the sales person of the given id or None if the id does not exist
         """
-        pass
+        for employee in self.sales_people:
+            if employee.get_id() == int(employee_id):
+                return employee
+        return None
+
+
 
     def get_sale_frequencies(self):
         """
--- assignments/hw11/sales_person.py
+++ assignments/hw11/sales_person.py
@@ -7,20 +7,19 @@ Certificate of Authenticity
 I certify that the work is entirely my own.
 """
 
+
 class SalesPerson:
     """
     Creates a employee with a name, id number, and a list of their sales
     """
-
-
 
     def __init__(self, employee_id, name):
         """
         Creates the employee and sales list default is an empty list
         """
 
-        self.employee_id = employee_id
-        self.employee_name = name
+        self.employee_id = int(employee_id)
+        self.name = str(name)
         self.sales = []
 
     def get_id(self):
@@ -33,27 +32,29 @@ class SalesPerson:
         """
         Sets the name of the employee and changes it
         """
-        self.employee_name = name
+        self.name = name
 
     def get_name(self):
         """
         Returns the name of the employee
         """
-        return self.employee_name
+        return self.name
 
     def enter_sale(self, sale):
         """
         Appends a sale value to the sales list
         """
-        self.sales.append(sale)
+        self.sales.append(float(sale))
 
     def total_sales(self):
         """
         Totals the number of sales from the list and returns them as a float value
         """
         sale_total = 0.0
+
         for sale in self.sales:
-            sale_total = sale_total + eval(sale)
+            sale_total = sale_total + sale
+
         return sale_total
 
     def get_sales(self):
@@ -67,7 +68,7 @@ class SalesPerson:
         Checks the employee and see's if their sales total meets the quota value entered
         """
         sale_total = self.total_sales()
-        if sale_total == quota:
+        if sale_total >= quota:
             return True
         return False
 
@@ -78,10 +79,9 @@ class SalesPerson:
         """
         if other.total_sales() < self.total_sales():
             return 1
-        elif other.total_sales() > self.total_sales():
-            return -1
-        elif other.total_sales() == self.total_sales():
+        if other.total_sales() == self.total_sales():
             return 0
+        return -1
 
     def __str__(self):
-        return '{}-{}:{}'.format(self.employee_id, self.employee_name, self.sales)
+        return '{}-{}:{}'.format(self.employee_id, self.name, self.total_sales())
